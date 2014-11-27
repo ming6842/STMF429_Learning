5d79b018dea55b3b16d2bae57d9f47f34512a2d0
ming6842 <ming6842@hotmail.com>
2014-11-27 17:56:04 +0800

    Drawing CE-91

--- stm32f429_LCD/main.c
+++ stm32f429_LCD/main.c
@@ -52,6 +52,17 @@ static inline void Delay_1us(uint32_t nCnt_1us)
     for (nCnt = 13; nCnt != 0; nCnt--);
 }
 
+void DrawThickCircle(uint32_t x,uint32_t y,uint32_t radius, uint32_t thickness){
+
+
+    LCD_SetTextColor(LCD_COLOR_BLACK);
+    LCD_DrawFullCircle(x, y, radius);
+    LCD_SetColors(LCD_COLOR_WHITE-1,LCD_COLOR_WHITE);
+    LCD_DrawFullCircle(x, y, radius-thickness);
+
+
+}
+
 int main(void)
 {
   uint8_t colorR =0 ,colorG =0 ,colorB =0 ;
@@ -115,18 +126,48 @@ int main(void)
 
     //LCD_SetTransparency(122);
     LCD_Clear(LCD_COLOR_WHITE);
+
+#define X_MIDDLE 120
+#define Y_MIDDLE 180
+
+
+
+
     LCD_SetTextColor(LCD_COLOR_BLACK);
-    LCD_DrawFullCircle(120, 240, 40);
-    Delay_1us(10000);
+    LCD_DrawUniLine(X_MIDDLE+75, Y_MIDDLE-75  , X_MIDDLE-75, Y_MIDDLE+75);
+    LCD_DrawUniLine(X_MIDDLE+75, Y_MIDDLE-75-1, X_MIDDLE-75, Y_MIDDLE+75-1);
+    LCD_DrawUniLine(X_MIDDLE+75, Y_MIDDLE-75+1, X_MIDDLE-75, Y_MIDDLE+75+1);
 
-    LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_WHITE);
-    LCD_DrawFullCircle(120, 240, 30);
+    LCD_DrawUniLine(X_MIDDLE-75, Y_MIDDLE-75  , X_MIDDLE+75, Y_MIDDLE+75);
+    LCD_DrawUniLine(X_MIDDLE-75, Y_MIDDLE-75-1, X_MIDDLE+75, Y_MIDDLE+75-1);
+    LCD_DrawUniLine(X_MIDDLE-75, Y_MIDDLE-75+1, X_MIDDLE+75, Y_MIDDLE+75+1);
 
+
+    LCD_DrawFullRect(X_MIDDLE-60,Y_MIDDLE-5,120,10);
+    LCD_DrawFullRect(X_MIDDLE-5,Y_MIDDLE-60,10,120);
+
+
+    DrawThickCircle(X_MIDDLE,Y_MIDDLE,30,7);
+
+    DrawThickCircle(X_MIDDLE+60,Y_MIDDLE,18,5);
+    DrawThickCircle(X_MIDDLE-60,Y_MIDDLE,18,5);
+    DrawThickCircle(X_MIDDLE   ,Y_MIDDLE+60,18,5);
+    DrawThickCircle(X_MIDDLE   ,Y_MIDDLE-60,18,5);
+
+    DrawThickCircle(X_MIDDLE+75,Y_MIDDLE+75,25,5);
+    DrawThickCircle(X_MIDDLE-75,Y_MIDDLE-75,25,5);
+    DrawThickCircle(X_MIDDLE+75,Y_MIDDLE-75,25,5);
+    DrawThickCircle(X_MIDDLE-75,Y_MIDDLE+75,25,5);
+
+    LCD_SetColors(LCD_COLOR_BLACK,LCD_COLOR_WHITE-1);
+
+    LCD_DisplayStringLine(LINE(1), (uint8_t*)"     CE-91     ");
 
     Delay_1us(10000);
 
     // LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_WHITE);
     // LCD_DrawFullRect(0,0,240,320);
+  LCD_SetLayer(LCD_BACKGROUND_LAYER);
   while (1)
   {
 
@@ -141,7 +182,7 @@ int main(void)
 
       colorR -= 1;
 
-      if(colorR<2) colorR_dir=1;
+      if(colorR<20) colorR_dir=1;
     }
 
     if(colorG_dir){
@@ -154,7 +195,7 @@ int main(void)
 
       colorG -= 2;
 
-      if(colorG<3) colorG_dir=1;
+      if(colorG<25) colorG_dir=1;
     }
 
     if(colorB_dir){
@@ -167,13 +208,12 @@ int main(void)
 
       colorB -= 3;
 
-      if(colorB<4) colorB_dir=1;
+      if(colorB<25) colorB_dir=1;
     }
 
 
-    // LCD_SetLayer(LCD_BACKGROUND_LAYER);
-    // LCD_SetColors(ASSEMBLE_RGB(colorR, colorG, colorB),LCD_COLOR_BLACK);
-    // LCD_DrawFullRect(0,0,240,320);
+    LCD_SetColors(ASSEMBLE_RGB(colorR, colorG, colorB),LCD_COLOR_BLACK);
+    LCD_DrawFullRect(0,0,240,320);
 
 
 
